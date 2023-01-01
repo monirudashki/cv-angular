18d30bd8d133c208801e493ffc4f0932377fb947
simeon.rudashki <simeon.rudashki@abv.bg>
2023-01-01 19:09:31 +0200

    last hope so

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/cv-angular'));
+app.use(express.static(__dirname + '/dist/cv-angular'));
 
 app.get('/*', function(req, res) {
-   res.sendFile(path.join(__dirname + 'cv-angular/index.html')); 
+   res.sendFile(path.join(__dirname + '/dist/cv-angular/index.html')); 
 });
 
 app.listen(process.env.PORT || 8080);
